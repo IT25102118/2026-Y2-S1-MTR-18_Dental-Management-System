ade0ddd6fe24a68f320011beb7e07ae5db03350f
IT25102118 <IT25102118@my.sliit.lk>
2026-08-21 14:27:29 +0530

    chore: initialize Spring Boot backend foundation

---
+++ backend/src/test/java/com/dentcare/DentcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
